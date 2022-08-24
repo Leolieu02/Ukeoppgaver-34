59f616a65072a6994d09ba36d97117ba230d70e8
Leolieu02 <s364570@oslomet.no>
2022-08-24 23:54:41 +0200

    Initial commit

---
+++ src/Oppgaver/HalloVerden.java
@@ -0,0 +1,14 @@
+package Oppgaver;
+
+public class HalloVerden {
+    public static void main(String... args) {
+        System.out.println("Hei AlgDat!");
+
+        System.out.println();
+
+        for (int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
+
